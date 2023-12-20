f8c74778a646ac58cb928037209c37ced1bb6c78
ky3ow <vova2341591@gmail.com>
2023-12-20 15:54:04 +0000

    Add .config/wezterm/lua/everforest.lua Add .config/wezterm/lua/linux.lua Add .config/wezterm/lua/mac.lua Add .config/wezterm/lua/theme.lua Add .config/wezterm/wezterm.lua

---
+++ dot_config/wezterm/lua/theme.lua
@@ -0,0 +1,15 @@
+local wezterm = require('wezterm')
+
+return {
+    configure = function(M)
+        M.colors = require('lua/everforest').colors()
+        M.window_frame = require('lua/everforest').window_frame()
+        M.window_background_opacity = 1.0
+        M.window_decorations = "RESIZE"
+
+        M.inactive_pane_hsb = {
+            brightness = 0.6,
+        }
+
+    end
+}
---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,58 @@
+local config = { keys = {}, unix_domains = {} }
+local wezterm = require('wezterm')
+local act = wezterm.action
+-- Theme --
+require('lua/theme').configure(config)
+
+-- Keys --
+config.keys = {
+    {
+        key = "j",
+        mods = "CTRL|SHIFT",
+        action = act.ActivatePaneDirection "Down",
+    },
+    {
+        key = "k",
+        mods = "CTRL|SHIFT",
+        action = act.ActivatePaneDirection "Up",
+    },
+    {
+        key = "h",
+        mods = "CTRL|SHIFT",
+        action = act.ActivatePaneDirection "Left",
+    },
+    {
+        key = "l",
+        mods = "CTRL|SHIFT",
+        action = act.ActivatePaneDirection "Right",
+    },
+    {
+        key = "s",
+        mods = "CTRL|SHIFT",
+        action = act.PaneSelect { alphabet = "123456789" },
+    },
+    {
+        key = "y",
+        mods = "CTRL|SHIFT",
+        action = act.QuickSelect
+    }
+}
+
+config.key_map_preference = "Physical"
+
+-- WSL --
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+    table.insert(config.unix_domains, { name = 'wsl', serve_command = { 'wsl', 'wezterm-mux-server', '--daemonize' } })
+end
+
+-- ShitBook --
+if wezterm.target_triple == "x86_64-apple-darwin" then
+    require('lua/mac').configure_mac(config)
+end
+
+-- Linux --
+if wezterm.target_triple == "x86_64-unknown-linux-gnu" then
+    require('lua/linux').configure_linux(config)
+end
+
+return config
