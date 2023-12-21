f497ae1dc8dc2b00957d65727368bee68b7c99a7
ky3ow <vova2341591@gmail.com>
2023-12-21 14:24:02 +0200

    Add .config/wezterm/lua/keyboard.lua Add .config/wezterm/lua/podman.lua

---
+++ dot_config/wezterm/lua/keyboard.lua
@@ -0,0 +1,44 @@
+local wezterm = require('wezterm')
+local act = wezterm.action
+return {
+	setup = function(M)
+		M.keys = {
+			{
+				key = "j",
+				mods = "CTRL|SHIFT",
+				action = act.ActivatePaneDirection "Down",
+			},
+			{
+				key = "k",
+				mods = "CTRL|SHIFT",
+				action = act.ActivatePaneDirection "Up",
+			},
+			{
+				key = "h",
+				mods = "CTRL|SHIFT",
+				action = act.ActivatePaneDirection "Left",
+			},
+			{
+				key = "l",
+				mods = "CTRL|SHIFT",
+				action = act.ActivatePaneDirection "Right",
+			},
+			{
+				key = "s",
+				mods = "CTRL|SHIFT",
+				action = act.PaneSelect { alphabet = "123456789" },
+			},
+			{
+				key = "y",
+				mods = "CTRL|SHIFT",
+				action = act.QuickSelect,
+			},
+			{
+				key = "l",
+				mods = "CTRL|SHIFT",
+				action = act.ShowLauncher,
+			}
+		}
+		M.key_map_preference = "Physical"
+	end
+}
---
+++ dot_config/wezterm/lua/podman.lua
@@ -0,0 +1,40 @@
+local wezterm = require('wezterm')
+local act = wezterm.action
+function podman_list()
+	local _,container_str,_ = wezterm.run_child_process { "podman", "container", "ls", "--format", "'{{.ID}}:{{.Names}}'" }
+	local containers = {}
+	for i, n in container_str:gmatch("(%w+):(%w+)") do
+		table.insert(containers, { id=i, name=n })
+	end
+	return containers
+end
+
+function make_fixup(id)
+	return function(cmd)
+		cmd.args = cmd.args or {}
+		local wrapped = { 'distrobox', 'enter', id }
+
+		 for _, arg in ipairs(cmd.args) do
+		 	table.insert(wrapped, arg)
+		 end
+
+		cmd.args = wrapped
+		print(cmd)
+		return cmd
+	end
+end
+
+return {
+	setup = function(M)
+		for _,container in pairs(podman_list()) do
+			table.insert(
+				M.exec_domains,
+				wezterm.exec_domain(
+					container.name,
+					make_fixup(container.id),
+					container.name
+				)
+			)
+		end
+	end
+}
