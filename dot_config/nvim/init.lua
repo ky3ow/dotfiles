a02eaa2ca37008617e8fb1460450ddaed74dc3c2
ky3ow <vova2341591@gmail.com>
2024-06-25 03:23:00 +0300

    filetypes for yaml and html jinja templates

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -33,6 +33,37 @@ vim.opt.rtp:prepend(lazypath)
 -- [[ Plugins ]] from lua/plugins directory
 require("lazy").setup("plugins")
 
+-- [[ Filetypes assosiation ]]
+
+-- extensions->filetype mapping
+-- filetype can be function(path, bufnr) -> ft
+local filetypes = {
+	[{'jinja', 'j2'}] = function(path, _)
+		local ft = "jinja"
+
+		if string.find(path, "%.html") then
+			ft = ft .. ".html"
+		end
+		if string.find(path, "%.yml") or string.find(path, "%.yaml") then
+			ft = ft .. ".yaml"
+		end
+		return ft
+	end,
+}
+
+local extension_table = {}
+for extensions, ft_func in pairs(filetypes) do
+	for _, ext in ipairs(extensions) do
+		extension_table[ext] = ft_func
+	end
+end
+
+
+vim.filetype.add({
+	extension = extension_table
+})
+
+
 -- [[ Highlight on yank ]]
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
