34d18a41e89c6d7ea2d8f021c6d5576f029ae2f2
ky3ow <vova2341591@gmail.com>
2024-07-18 18:52:06 +0300

    poggers folds

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -2,9 +2,9 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-require("config.options").setup {}
-require("config.filetypes").setup {}
-require("config.keymaps").setup {}
-require("config.folds").setup {}
+require("config.options").setup({})
+require("config.filetypes").setup({})
+require("config.keymaps").setup({})
+require("config.folds").setup({})
 
-require("config.lazy").setup {}
+require("config.lazy").setup({})
--- dot_config/nvim/lua/config/folds.lua
+++ dot_config/nvim/lua/config/folds.lua
@@ -1,17 +1,29 @@
 return {
 	setup = function(_)
+		local function set_fold_hl()
+			local cl = vim.api.nvim_get_hl(0, { name = "CursorLineNr" })
+			local txt = vim.api.nvim_get_hl(0, { name = "@text" })
+			vim.api.nvim_set_hl(0, "FoldedText", { bg = cl.bg, fg = cl.fg })
+			vim.api.nvim_set_hl(0, "FoldedIcon", { fg = txt.fg })
+		end
+
+		set_fold_hl()
+		vim.api.nvim_create_autocmd("ColorScheme", {
+			callback = set_fold_hl,
+		})
+
 		FoldText = function()
 			local lines = vim.v.foldend - vim.v.foldstart + 1
-			local width = vim.api.nvim_win_get_width(0) - vim.fn.getwininfo(vim.fn.win_getid())[1].textoff
 
 			local start_content = table.concat(vim.fn.getbufline(vim.api.nvim_get_current_buf(), vim.v.foldstart))
 			local indent, text = start_content:match("^(%s*)(%S.*)")
 			local spaces = string.rep(" ", vim.fn.strdisplaywidth(indent))
 
-			local marker = spaces .. "[>] " .. text .. " [" .. lines .. "]"
-			return marker .. string.rep(" ", width - vim.fn.strchars(marker))
+			local marker =
+				{ { spaces .. "> ", "FoldedIcon" }, { text }, { "[" }, { tostring(lines), "FoldedText" }, { "]" } }
+			return marker
 		end
 
-		vim.o.foldtext = "v:lua.FoldText()"
+		vim.opt.foldtext = "v:lua.FoldText()"
 	end,
 }
