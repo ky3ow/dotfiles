bb1dd4023de12fa5ff5a7f55bfaec79cfd966b28
ky3ow <vova2341591@gmail.com>
2025-01-03 09:49:34 +0200

    we woo

--- dot_config/emacs/early-init.el
+++ dot_config/emacs/early-init.el
@@ -15,8 +15,8 @@
 (setq gc-cons-threshold most-positive-fixnum
       gc-cons-percentage 0.5)
 
-(defvar ky3ow-emacs--file-name-handler-alist file-name-handler-alist)
-(defvar ky3ow-emacs--vc-handled-backends vc-handled-backends)
+(defvar my-emacs--file-name-handler-alist file-name-handler-alist)
+(defvar my-emacs--vc-handled-backends vc-handled-backends)
 
 (setq file-name-handler-alist nil
       vc-handled-backends nil)
@@ -25,21 +25,21 @@
           (lambda ()
             (setq gc-cons-threshold (* 1000 1000 8)
                   gc-cons-percentage 0.1
-                  file-name-handler-alist ky3ow-emacs--file-name-handler-alist
-                  vc-handled-backends ky3ow-emacs--vc-handled-backends)))
+                  file-name-handler-alist my-emacs--file-name-handler-alist
+                  vc-handled-backends my-emacs--vc-handled-backends)))
 
 (require 'xdg)
 
-(defcustom ky3ow-emacs-state (expand-file-name "emacs" (xdg-state-home))
+(defcustom my-emacs-state (expand-file-name "emacs" (xdg-state-home))
   "Specifies where to store state files(undo, backups)")
-(defcustom ky3ow-emacs-cache (expand-file-name "emacs" (xdg-cache-home))
+(defcustom my-emacs-cache (expand-file-name "emacs" (xdg-cache-home))
   "Specifies where to store cache files(eln cache)")
-(defcustom ky3ow-emacs-data (expand-file-name "emacs" (xdg-data-home))
+(defcustom my-emacs-data (expand-file-name "emacs" (xdg-data-home))
   "Specifies where to store data files(packages from elpa)")
 
-(make-directory ky3ow-emacs-state t)
-(make-directory ky3ow-emacs-cache t)
-(make-directory ky3ow-emacs-data t)
+(make-directory my-emacs-state t)
+(make-directory my-emacs-cache t)
+(make-directory my-emacs-data t)
 
-(startup-redirect-eln-cache ky3ow-emacs-cache)
-(setq package-user-dir (expand-file-name "packages" ky3ow-emacs-data))
+(startup-redirect-eln-cache my-emacs-cache)
+(setq package-user-dir (expand-file-name "packages" my-emacs-data))
