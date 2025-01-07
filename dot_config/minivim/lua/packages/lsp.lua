1ec906db61c95fbdddaaf1f987be7a8a74a5e022
ky3ow <vova2341591@gmail.com>
2025-01-07 02:33:06 +0200

    lsp things

--- dot_config/minivim/lua/packages/lsp.lua
+++ dot_config/minivim/lua/packages/lsp.lua
@@ -1,35 +1,3 @@
-local H = {}
-
-function H.lsp_attach(_, buf)
-	local telescope = require "telescope.builtin"
-	local map = function(mode, keys, func, desc)
-		vim.keymap.set(mode, keys, func, { buffer = buf, desc = desc })
-	end
-
-	local formatprg = vim.fn.exists(":Format") == 2 and vim.cmd.Format or vim.lsp.buf.format -- exists matches fully(==2)
-
-	map("n", "<leader>lr", vim.lsp.buf.rename, "[L]SP [R]ename")
-	map("n", "<leader>la", vim.lsp.buf.code_action, "[L]sp Code [A]ction")
-	map("n", "<leader>lf", formatprg, "[L]SP [F]ormat")
-	map("n", "<leader>ls", telescope.lsp_document_symbols, "[L]SP document [S]ymbols")
-
-	map("n", "gd", telescope.lsp_definitions, "[G]oto [D]efinition")
-	map("n", "gr", telescope.lsp_references, "[G]oto [R]eferences")
-	map("n", "gI", telescope.lsp_implementations, "[G]oto [I]mplementation")
-	map("n", "<leader>D", telescope.lsp_type_definitions, "Type [D]efinition")
-
-	map("n", "K", vim.lsp.buf.hover, "Hover Documentation")
-	map({ "n", "i" }, "<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
-
-	map("n", "gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-	map("n", "<leader>ws", telescope.lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-	map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-	map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-	map("n", "<leader>wl", function()
-		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	end, "[W]orkspace [L]ist Folders")
-end
-
 local add = require("mini.deps").add
 local now, later = require("mini.deps").now, require("mini.deps").later
 
@@ -44,25 +12,58 @@ add {
 	}
 }
 
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("ky3ow.LspAttach", { clear = true }),
+	callback = function(e)
+		local telescope = require "telescope.builtin"
+		local map = function(mode, keys, func, desc)
+			vim.keymap.set(mode, keys, func, { buffer = e.buf, desc = desc })
+		end
+
+		local formatprg = vim.fn.exists(":Format") == 2 and vim.cmd.Format or vim.lsp.buf
+			.format -- exists matches fully(==2)
+
+		map("n", "<leader>lr", vim.lsp.buf.rename, "[L]SP [R]ename")
+		map("n", "<leader>la", vim.lsp.buf.code_action, "[L]sp Code [A]ction")
+		map("n", "<leader>lf", formatprg, "[L]SP [F]ormat")
+		map("n", "<leader>ls", telescope.lsp_document_symbols, "[L]SP document [S]ymbols")
+
+		map("n", "gd", telescope.lsp_definitions, "[G]oto [D]efinition")
+		map("n", "gr", telescope.lsp_references, "[G]oto [R]eferences")
+		map("n", "gI", telescope.lsp_implementations, "[G]oto [I]mplementation")
+		map("n", "<leader>D", telescope.lsp_type_definitions, "Type [D]efinition")
+
+		map("n", "K", vim.lsp.buf.hover, "Hover Documentation")
+		map({ "n", "i" }, "<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+
+		map("n", "gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+		map("n", "<leader>ws", telescope.lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+		map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+		map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+		map("n", "<leader>wl", function()
+			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		end, "[W]orkspace [L]ist Folders")
+	end
+})
+
 now(function()
 	local blink_installed, blink = pcall(require, "blink.cmp")
 
 	require("mason").setup {}
-	require("mason-lspconfig").setup {
-		handlers = {
-			function(server_name)
-				local config = vim.g.settings.language_servers[server_name] or {}
-				if blink_installed then
-					config.capabilities = blink.get_lsp_capabilities(config.capabilities, true)
-				else
-					config.capabilities = vim.tbl_deep_extend("force", {}, vim.lsp.protocol.make_client_capabilities(), config.capabilities or {})
-				end
+	for server, config in pairs(vim.g.settings.language_servers) do
+		if blink_installed then
+			config.capabilities = blink.get_lsp_capabilities(config.capabilities, true)
+		else
+			config.capabilities = vim.tbl_deep_extend(
+				"force",
+				{},
+				vim.lsp.protocol.make_client_capabilities(),
+				config.capabilities or {}
+			)
+		end
 
-				config.on_attach = H.lsp_attach
-				require("lspconfig")[server_name].setup(config)
-			end
-		}
-	}
+		require("lspconfig")[server].setup(config)
+	end
 end)
 
 later(function()
