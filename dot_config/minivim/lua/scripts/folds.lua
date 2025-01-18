1f6b7be7336460608c491d98fc81d0f1d21a2c91
ky3ow <vova2341591@gmail.com>
2025-01-18 04:05:40 +0200

    prepare to moving to plugin/

--- dot_config/minivim/lua/scripts/folds.lua
+++ dot_config/minivim/lua/scripts/folds.lua
@@ -1,21 +1,13 @@
-local M = {}
-local H = {}
+vim.api.nvim_create_autocmd({ "ColorScheme", "VimEnter" }, {
+	group = vim.api.nvim_create_augroup("Ky3ow.FoldHighlights", { clear = true }),
+	callback = function()
+		local cl = vim.api.nvim_get_hl(0, { name = "Number" })
 
-function H.set_fold_hl()
-	local cl = vim.api.nvim_get_hl(0, { name = "Number" })
-
-	vim.api.nvim_set_hl(0, "FoldedText", { bg = cl.bg, fg = cl.fg })
-end
-
-H.set_fold_hl()
-vim.api.nvim_create_autocmd("ColorScheme", {
-	callback = H.set_fold_hl,
+		vim.api.nvim_set_hl(0, "FoldedText", { bg = cl.bg, fg = cl.fg })
+	end,
 })
 
-vim.opt.foldtext = 'v:lua.require("scripts.folds").fold_text()'
-vim.opt.foldopen:remove "block"
-
-M.fold_text = function()
+vim.g.fold_text = function()
 	local lines = vim.v.foldend - vim.v.foldstart + 1
 
 	local start_content = table.concat(vim.fn.getbufline(vim.api.nvim_get_current_buf(), vim.v.foldstart))
@@ -23,14 +15,15 @@ M.fold_text = function()
 	local spaces = string.rep(" ", vim.fn.strdisplaywidth(indent))
 
 	local marker = {
-		{ spaces .. "> ", "FoldedText" },
+		{ spaces .. "> ",  "FoldedText" },
 		{ text },
 		{ "[" },
 		{ tostring(lines), "FoldedText" },
 		{ "]" },
-		{ "...", "FoldedText" },
+		{ "...",           "FoldedText" },
 	}
 	return marker
 end
 
-return M
+vim.opt.foldtext = 'g:fold_text()'
+vim.opt.foldopen:remove "block"
