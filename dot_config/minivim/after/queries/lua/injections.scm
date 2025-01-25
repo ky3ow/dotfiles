3e10035235c221599d6787831d99d50b525890af
ky3ow <vova2341591@gmail.com>
2025-01-25 23:28:46 +0200

    add ts query for <cmd>Commands<cr> mappings

---
+++ dot_config/minivim/after/queries/lua/injections.scm
@@ -0,0 +1,7 @@
+;;extends
+
+(string
+  content: _ @injection.content
+  (#lua-match? @injection.content "^<cmd>.*<cr>")
+  (#offset! @injection.content 0 5 0 -4 0) ;; match_id start_col start_row end_col end_row
+  (#set! injection.language "vim"))
