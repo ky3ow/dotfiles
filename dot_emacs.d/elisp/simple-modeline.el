080175dd1b5c6e8c0cd63922338280a4e00408ce
ky3ow <vova2341591@gmail.com>
2025-03-30 21:11:47 +0000

    improve modeline

--- dot_emacs.d/elisp/simple-modeline.el
+++ dot_emacs.d/elisp/simple-modeline.el
@@ -105,7 +105,7 @@ Specific to the current window's mode line.")
   (let ((filename (buffer-file-name)))
     (format "%s%s%s" (if buffer-read-only
 			 (format "%s " (char-to-string #xE0A2)) "")
-	    (or (abbreviate-file-name filename) (buffer-name))
+	    (if filename (abbreviate-file-name filename) (buffer-name))
 	    (if (and filename (buffer-modified-p)) "*" ""))))
 
 (defun simple-modeline-buffer-name-help-echo ()
@@ -130,16 +130,152 @@ face.  Let other buffers have no face.")
 
 (put 'simple-modeline-buffer-module 'risky-local-variable t)
 
+;;; Major mode
+
+(defface simple-modeline-indicator-red
+  `((default :inherit (bold simple-modeline-indicator-button)
+	     :foregound ,(face-attribute 'ansi-color-red :foreground)))
+  "Face for modeline indicators"
+  :group 'simple-modeline-faces)
+
+(defun simple-modeline-major-mode-indicator ()
+  "Return appropriate propertized mode line indicator for the major mode."
+  (let ((indicator (cond
+                    ((derived-mode-p 'text-mode) "§")
+                    ((derived-mode-p 'prog-mode) "λ")
+                    ((derived-mode-p 'comint-mode) ">_")
+                    (t "🞊"))))
+    (propertize indicator 'face 'shadow)))
+
+(defun simple-modeline-major-mode-name ()
+  "Return capitalized `major-mode' without the -mode suffix."
+  (capitalize (string-replace "-mode" "" (symbol-name major-mode))))
+
+(defun simple-modeline-major-mode-help-echo ()
+  "Return `help-echo' value for `simple-modeline-major-mode'."
+  (if-let* ((parent (get major-mode 'derived-mode-parent)))
+      (format "Symbol: `%s'.  Derived from: `%s'" major-mode parent)
+    (format "Symbol: `%s'." major-mode)))
+
+(defvar-local simple-modeline-major-module
+    (list
+     (propertize "%[" 'face 'simple-modeline-indicator-red)
+     '(:eval
+       (concat
+        (simple-modeline-major-mode-indicator)
+        " "
+        (propertize
+	 (simple-modeline-major-mode-name)
+         'mouse-face 'mode-line-highlight
+         'help-echo (simple-modeline-major-mode-help-echo))))
+     (propertize "%]" 'face 'simple-modeline-indicator-red))
+  "Mode line construct for displaying major modes.")
+
+(defvar-local simple-modeline-process-module
+    (list '("" mode-line-process))
+  "Mode line construct for the running process indicator.")
+
+(put 'simple-modeline-major-module 'risky-local-variable t)
+(put 'simple-modeline-process-module 'risky-local-variable t)
+
+;;; LSP
+
+(with-eval-after-load 'eglot
+  (setq mode-line-misc-info
+        (delete '(eglot--managed-mode (" [" eglot--mode-line-format "] ")) mode-line-misc-info)))
+
+(defvar-local simple-modeline-lsp-module
+    `(:eval
+      (when (and (featurep 'eglot) (mode-line-window-selected-p))
+        '(eglot--managed-mode eglot--mode-line-format)))
+  "Mode line construct displaying Eglot information.
+Specific to the current window's mode line.")
+
+(put 'simple-modeline-lsp-module 'risky-local-variable t)
+
+;;; Misc
+
+(defvar-local simple-modeline-misc-module
+    '(:eval
+      (when (mode-line-window-selected-p)
+        mode-line-misc-info))
+  "Mode line construct displaying `mode-line-misc-info'.
+Specific to the current window's mode line.")
+
+(put 'simple-modeline-misc-module 'risky-local-variable t)
+
+;;; Lines
+
+(defvar-local simple-modeline-lines-module
+    '(:eval
+       (propertize (concat " [%l/" (number-to-string (line-number-at-pos (point-max))) "] ")
+                    'face 'simple-modeline-indicator-blue-bg
+                    'mouse-face 'mode-line-highlight))
+  "Display lines")
+
+(put 'simple-modeline-lines-module 'risky-local-variable t)
+
+;;; Encoding
+
+(defface simple-modeline-indicator-magenta-bg
+  `((default :inherit (bold simple-modeline-indicator-button)
+	     :background ,(face-attribute 'ansi-color-magenta :background)
+	     :foreground "black"))
+  "Face for modeline indicators"
+  :group 'simple-modeline-faces)
+
+(defvar-local simple-modeline-encoding-module
+    '(:eval
+       (propertize (format " %s " (symbol-name (or buffer-file-coding-system 'utf-8-unix)))
+                    'face 'simple-modeline-indicator-magenta-bg
+                    'mouse-face 'mode-line-highlight))
+  "File encoding")
+
+(put 'simple-modeline-encoding-module 'risky-local-variable t)
+
+;;; Minor
+
+(defface simple-modeline-indicator-yellow-bg
+  `((default :inherit (bold simple-modeline-indicator-button)
+	     :background ,(face-attribute 'ansi-color-yellow :background)
+	     :foreground "black"))
+  "Face for modeline indicators"
+  :group 'simple-modeline-faces)
+
+(defvar-local simple-modeline-minor-module
+    '(:eval
+       (propertize (format-mode-line minor-mode-alist)
+                    'face 'simple-modeline-indicator-yelow-bg
+                    'mouse-face 'mode-line-highlight))
+  "File encoding")
+
+(put 'simple-modeline-minor-module 'risky-local-variable t)
+
+;;; Align
+
+
 ;;; Setting the modeline
 
 (setq-default mode-line-format
 	      '("%e"
+		simple-modeline-input-module
 		simple-modeline-kmacro-module
 		simple-modeline-narrow-module
 		simple-modeline-remote-module
-		simple-modeline-input-module
 		" "
 		simple-modeline-buffer-module
+		" "
+		simple-modeline-minor-module
+		" "
+                simple-modeline-lsp-module
+		;; mode-line-format-right-align ; Emacs 30
+                " "
+		simple-modeline-major-module
+		" "
+		simple-modeline-process-module
+		simple-modeline-misc-module
+		simple-modeline-lines-module
+		simple-modeline-encoding-module
 		))
 
 (provide 'simple-modeline)
