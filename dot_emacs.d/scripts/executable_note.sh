c330fb35f11d4a84029ea329daa8a12a4c3f847e
ky3ow <vova2341591@gmail.com>
2026-07-11 20:58:12 +0300

    shortcut for opening scratchpad

---
+++ dot_emacs.d/scripts/executable_note.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+EXIST=$(emacsclient -e '(if (filtered-frame-list (lambda (f) (string= (frame-parameter f '\''name) "notes"))) 1 0 )')
+
+if [ $EXIST -eq 1 ]; then
+  emacsclient -e '
+    (let ((frame (car (filtered-frame-list (lambda (f) (string= (frame-parameter f '\''name) "notes"))))))
+        (make-frame-invisible frame)
+        (make-frame-visible frame)
+        (find-file "~/Notes/scratchpad.org")
+        (end-of-buffer))'
+else
+  emacsclient -c -n -F "((name . \"notes\") (width . 100) (height . 40))" -e '
+      (find-file "~/Notes/scratchpad.org")
+      (end-of-buffer)'
+fi
